07b3b736e3ab41f22edf0b484e01fe2c30308004
DrakeCaesar <dominik.cisar@gmail.com>
2024-07-27 21:02:46 +0200

    tooltip

--- src/main.ts
+++ src/main.ts
@@ -20,4 +20,36 @@ tags.forEach((tag) => {
     </div>
   `;
   tagsContainer?.appendChild(tagElement);
+
+  // Add event listener to position the tooltip
+  tagElement.addEventListener("mousemove", function (e: MouseEvent) {
+    const tooltip = this.querySelector(".tag-tooltip") as HTMLDivElement;
+    tooltip.style.left = `${e.clientX + 10}px`;
+    tooltip.style.top = `${e.clientY + 10}px`;
+    tooltip.style.visibility = "visible";
+    tooltip.style.opacity = "1";
+  });
+
+  tagElement.addEventListener("mouseleave", function () {
+    const tooltip = this.querySelector(".tag-tooltip") as HTMLDivElement;
+    tooltip.style.visibility = "hidden";
+    tooltip.style.opacity = "0";
+  });
+});
+
+// Add event listener to position the tooltip
+document.querySelectorAll(".tag").forEach((tag) => {
+  tag.addEventListener("mousemove", function (e: MouseEvent) {
+    const tooltip = this.querySelector(".tag-tooltip");
+    tooltip.style.left = `${e.clientX + 10}px`;
+    tooltip.style.top = `${e.clientY + 10}px`;
+    tooltip.style.visibility = "visible";
+    tooltip.style.opacity = "1";
+  });
+
+  tag.addEventListener("mouseleave", function () {
+    const tooltip = this.querySelector(".tag-tooltip");
+    tooltip.style.visibility = "hidden";
+    tooltip.style.opacity = "0";
+  });
 });
