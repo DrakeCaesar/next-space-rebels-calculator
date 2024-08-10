2be94640fe44bdbdd23dc780f2799a8ffad26f80
DrakeCaesar <dominik.cisar@gmail.com>
2024-08-10 14:51:35 +0200

    refactor: Remove vite-plugin-static-copy from vite.config.ts and package.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,6 @@
 import { defineConfig } from "vite";
-import { viteStaticCopy } from "vite-plugin-static-copy";
 
 export default defineConfig({
-  plugins: [
-    viteStaticCopy({
-      targets: [
-        {
-          src: "output.wasm", // Adjust the path to your actual wasm file location
-          dest: "", // Destination directory within the output directory
-        },
-      ],
-    }),
-  ],
   build: {
     outDir: "dist",
     sourcemap: true,
